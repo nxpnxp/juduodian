4c281b0a393966046d994a0896d0d2d6b3da8a3c
nxp913 <nxp913@sina.com>
2017-10-27 16:54:35 +0800

    nxp
    红包分离

--- Application/Nn/Controller/PubController.class.php
+++ Application/Nn/Controller/PubController.class.php
@@ -16,6 +16,7 @@ class PubController extends Controller {
 		);
 		$rr = M('DocumentShop')->where('id='.$info['id'])->save($data);
 		
+		echo 'success';
 	}
 	
 	public function hbpay_success(){
@@ -24,12 +25,53 @@ class PubController extends Controller {
 		$ordersn = I('ordersn');
 		$total_fee = I('total_fee');
 		$info = M('Wxhb')->where('sn="'.$ordersn.'"')->find();
+		if(!$info){ return false; }
+		
 		$data = array(
 			'ispay' => 1,
 			'paytype' => 1,
 			'paymoney' => $total_fee/100
 		);
 		$rr = M('Wxhb')->where('id='.$info['id'])->save($data);
+		
+		//天数大于1的  按 24小时计算  
+		//天数等于1的  按后台设置的期限计算
+		if($info['num'] > 1){
+			
+			if($info['isson'] == '0'){			
+				//生成子红包数据
+				$gettime = $info['gettime'];
+				$endtime = $gettime + 24*3600;
+				for ($i=0; $i < $info['num']; $i++) { 
+					$data = array(
+						'hbid' => $info['id'],
+						'shopid' => $info['shopid'],
+						'everymoney' => $info['everymoney'],
+						'yue' => $info['everymoney'],
+						'type' => $info['type'],
+						'ptmoney' => $info['ptmoney'],
+						'psqmoney1' => $info['psqmoney1'],
+						'psqmoney2' => $info['psqmoney2'],
+						'iskl' => $info['iskl'],
+						'kl' => $info['kl'],
+						'gettime' => $gettime,
+						'endtime' => $endtime,
+						'area' => $info['area']
+					);
+					M('WxhbSon')->add($data);
+					$gettime = $endtime;
+					$endtime = $gettime + 24*3600;
+				}
+				
+				//放置重复插入
+				M('Wxhb')->where('id='.$info['id'])->save(array('isson'=>1));
+				
+			}
+			
+		}
+		
+		echo 'success';
+		
 		
 	}
 
