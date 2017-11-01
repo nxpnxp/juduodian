102b516308460b88be10bcfa1e8662025cde9fa6
nxp913 <nxp913@sina.com>
2017-11-01 17:45:46 +0800

    nxp
    申请提现

--- Application/Nn/Controller/MemberController.class.php
+++ Application/Nn/Controller/MemberController.class.php
@@ -79,4 +79,76 @@ class MemberController extends HomeController {
 		$this->assign('collections',$collections);
 		$this->display();
 	}
+	
+	public function yuetixian(){
+    	
+		$openid = $this->openid;
+		$user = M('WxuserCode')->where(array('openid'=>$openid))->find();
+		$this->assign('user',$user);
+		
+		$tixian_min_money = M('Config')->where('id=43')->getField('value');
+		$this->assign('tixian_min_money',$tixian_min_money);
+		
+		//判断是否关注
+		if($user['subscribe'] != '1'){
+			$this->error('未关注，请关注后继续！');
+		}
+		//判断是否绑手机号
+		if($user['isbind'] != '1'){
+			$this->error('未绑手机号，请绑定后继续！');
+		}
+		
+		$this->display();
+	}
+	
+	public function doapply_tixian(){
+		$openid = $this->openid;
+		$user = M('WxuserCode')->where(array('openid'=>$openid))->find();
+		
+		$time = time();
+		$_time = date('Y-m-d',$time);
+		$time_min = strtotime($_time.' 00:00:00');
+		$time_max = strtotime($_time.' 23:59:59');
+		$search = M('WxuserTixian')
+					->where('uid='.$user['id'].' and applytime>='.$time_min.' and applytime<'.$time_max)
+					->find();
+		if($search){
+			if($search['type'] == '0'){
+				$this->error('您今日已经申请提现，请耐心等待审核！');
+			}
+			if($search['type'] == '1'){
+				$this->error('您今日申请提现已成功！');
+			}
+			if($search['type'] == '2'){
+				$this->error('您今日申请提现被拒绝！');
+			}
+		}
+		
+		$fee = I('post.fee');
+		$fee = intval($fee); 
+		
+		$tixian_min_money = M('Config')->where('id=43')->getField('value');
+		if($fee < $tixian_min_money){
+			$this->error('您申请的金额过小');
+		}
+		
+		if($fee > $user['yue']){
+			$this->error('您申请的金额超过您已有余额');
+		}
+		
+		$data = array(
+			'uid' => $user['id'],
+			'applyfee' => $fee,
+			'applytime' => $time,
+			'type' => 0 //0已申请  1审核通过并打款  2拒绝
+		);
+		$res = M('WxuserTixian')->add($data);
+		if($res){
+			$this->success('申请提现成功');
+		}else{
+			$this->error('申请提现失败');
+		}
+		
+	}
+	
 }
