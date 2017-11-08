99a0c4f91bb5fab9b5414a2eff05f09dcaf64b06
zxh5566 <1045746362@qq.com>
2017-11-08 16:27:14 +0800

    11/8

--- Application/Nn/Controller/IndexController.class.php
+++ Application/Nn/Controller/IndexController.class.php
@@ -20,14 +20,46 @@ class IndexController extends HomeController {
 		$this->assign('cates',$cates);
 		
 		$morens = M('Document')->alias('d')
-			->field('d.id,d.title,p.path,ds.longitude,ds.latitude')
+			->field('d.id,d.title,d.description,p.path,ds.longitude,ds.latitude,ds.showaddress')
 			->join('left join onethink_picture p on d.cover_id=p.id')
 			->join('left join onethink_document_shop ds on ds.id=d.id')
 			->where('d.status=1')
 			->limit(10)
 			->select();
+		foreach($morens as $k=>$v){
+			$morens[$k]['collection'] = M("Collection")->where(array('sid'=>$v['id']))->count();
+			$morens[$k]['zan'] = M("Zan")->where(array('sid'=>$v['id']))->count();
+		}
 		$this->assign('morens',$morens);
 		
+		//附近的店铺
+		$visit = M("WxuserLatlon")->where(array('uid'=>$user['id']))->order("time desc")->find();
+		$dianpu = M('Document')->alias('d')
+			->field('d.id,d.title,d.description,p.path,ds.longitude,ds.latitude,ds.showaddress')
+			->join('left join onethink_picture p on d.cover_id=p.id')
+			->join('left join onethink_document_shop ds on ds.id=d.id')
+			->where('d.status=1')
+			->select();
+		foreach($dianpu as $k=>$v){
+			$dianpu[$k]['juli'] = $this->getDistance($v['longitude'], $v['latitude'],$visit['lon'], $visit['lat']);
+		}
+
+		foreach ($dianpu as $key => $row)
+		{
+			$volume[$key]  = $row['juli'];
+			$edition[$key] = $row['id'];
+		}
+	
+		array_multisort($volume, SORT_ASC, $edition, SORT_ASC, $dianpu);
+	    
+		$dianpu1 = array_slice($dianpu,0,10);
+		foreach($dianpu1 as $k=>$v){
+			$dianpu1[$k]['collection'] = M("Collection")->where(array('sid'=>$v['id']))->count();
+			$dianpu1[$k]['zan'] = M("Zan")->where(array('sid'=>$v['id']))->count();
+		}
+		$this->assign('fujin',$dianpu1);
+
+
 		$script = &  load_wechat('Script');
 		$thisurl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
 		$options = $script->getJsSign($thisurl);
@@ -43,6 +75,34 @@ class IndexController extends HomeController {
 		
     	
 	}
+		
+		
+		private function getDistance($longitude1, $latitude1, $longitude2, $latitude2, $unit=2, $decimal=2){
+	
+	    $EARTH_RADIUS = 6370.996; // 地球半径系数
+	    $PI = 3.1415926;
+	
+	    $radLat1 = $latitude1 * $PI / 180.0;
+	    $radLat2 = $latitude2 * $PI / 180.0;
+	
+	    $radLng1 = $longitude1 * $PI / 180.0;
+	    $radLng2 = $longitude2 * $PI /180.0;
+	
+	    $a = $radLat1 - $radLat2;
+	    $b = $radLng1 - $radLng2;
+	
+	    $distance = 2 * asin(sqrt(pow(sin($a/2),2) + cos($radLat1) * cos($radLat2) * pow(sin($b/2),2)));
+	    $distance = $distance * $EARTH_RADIUS * 1000;
+	
+	    if($unit==2){
+	        $distance = $distance / 1000;
+	    }
+	
+	    return round($distance, $decimal);
+	
+	}	
+		
+		
 			
 	private function _request($url, $https=false, $method='get', $data=null)
 	{
